b18f32b9e495978e39b50c959cc6960f5e61a0b6
Blake <blakeshef1@gmail.com>
2019-04-01 13:35:08 -0500

    inital setup

---
+++ morningcoffee.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# morningcoffe script
+# author: Blake
+# like the first cup of coffee at work, just for your pc
+
+main () {
+	sudo apt update
+	sudo apt upgrade
+}
+
+main '$@'
---
+++ readme.md
@@ -0,0 +1,4 @@
+# morningcoffee
+
+small dumb script to get the pc up and running in the morning.
+
