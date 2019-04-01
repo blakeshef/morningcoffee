8b43ac28698daa03805b0c4660e5d11e9e553153
Blake <blakeshef1@gmail.com>
2019-04-01 18:55:18 -0500

    added askToUpgrade framework

--- morningcoffee.sh
+++ morningcoffee.sh
@@ -5,9 +5,30 @@
 # like the first cup of coffee at work, just for your pc
 
 main () {
-	echo "Good Morning! Here is your digital coffee."
+	printf "Good Morning! Here is your digital coffee.\n\n"
 	sudo apt -qq update
-	sudo apt upgrade
+	askToUpgrade
+}
+
+askToUpgrade () {
+	while true
+	do
+		printf "\n"
+		printf "Would you like to update these packages?"
+		read ask
+		if [[ $ask == "y" ]] || [[ $ask == "Y" ]]
+		then
+			sudo apt upgrade
+			break
+		elif [[ $ask == "n" ]] || [[ $ask == "N" ]]
+		then
+			printf "\nNo Problem!\n\n"
+			break
+		else
+			printf "\nI don't understand\n"
+		fi
+	done
+
 }
 
 main '$@'
