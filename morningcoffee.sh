5f52bdf31dea19dcf0d4146b7f08ecbd39dbc22e
Blake <blakeshef1@gmail.com>
2019-04-02 15:22:39 -0500

    added comments to AskToUpgrade

--- morningcoffee.sh
+++ morningcoffee.sh
@@ -11,20 +11,31 @@ main () {
 }
 
 askToUpgrade () {
+	# function that asks the user for permission to upgrade pagages that have updates
+	
+	# sets up a loop that will exit upon sucsessfull input
 	while true
 	do
 		printf "\n"
-		printf "Would you like to update these packages?"
+		printf "Would you like to update these packages? (y/n) "
+		
+		# grab input
 		read ask
+
+		# checks if the input for validity
 		if [[ $ask == "y" ]] || [[ $ask == "Y" ]]
 		then
+			# does an update with apt
 			sudo apt upgrade
+			# breaks out of the loop
 			break
 		elif [[ $ask == "n" ]] || [[ $ask == "N" ]]
 		then
 			printf "\nNo Problem!\n\n"
+			# breaks out of the loop
 			break
 		else
+			# prints error message and loops
 			printf "\nI don't understand\n"
 		fi
 	done
